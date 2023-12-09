58f28edbbab2da30b8b810545bb1fdd7d639f711
shmuelstav2 <shmhad123@gmail.com>
2023-12-09 23:54:50 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-
-// app.js
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const xlsx = require('xlsx');
+const { MongoClient } = require('mongodb');
 const path = require('path');
 
 const app = express();
@@ -13,27 +11,132 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true }));
 
 // Serve static files (like Excel file)
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files (HTML, CSS, etc.)
+app.use(express.static(path.join(__dirname, 'Login_v2')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'login.html'));
+    res.sendFile(path.join(__dirname, 'Login_v2/index.html'));
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
     // Check if the provided username and password match the hardcoded values
     if (username === 'lulim' && password === '12345') {
         // Authentication successful
-        // Read Excel file and render table
-        const excelFilePath = 'C:\\Users\\User\\Dropbox\\BMC\\prod\\current active flocks\\status.xlsx';
-        const workbook = xlsx.readFile(excelFilePath);
-        const sheetName = workbook.SheetNames[0];
-        const sheetData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
+        try {
+            // Connect to MongoDB
+            const mongoUri = 'mongodb://localhost:27017/';
+            const dbName = 'lulim';
+            const collectionName = 'tmuta_end';
 
-        // Render the table with a headline
-        const tableHtml = `<h1>תמותה</h1>${generateTableHtml(sheetData)}`;
-        res.send(tableHtml);
+            const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+            await client.connect();
+            const db = client.db(dbName);
+            const collection = db.collection(collectionName);
+
+            // Fetch data from MongoDB excluding _id field
+            // Fetch data from MongoDB excluding _id field
+            const mongoData = await collection.find({}, { projection: { _id: 0 } }).toArray();
+
+// Format the date as dd/mm/yyyy
+            const formattedData = mongoData.map(item => {
+                if (item.date instanceof Date) {
+                    item.date = item.date.toLocaleDateString('en-GB');  // Use 'en-GB' for dd/mm/yyyy format
+                }
+                return item;
+            });
+            function generateTableHtml(data) {
+                if (data.length === 0) {
+                    return '<p>No data available</p>';
+                }
+
+                const headers = Object.keys(data[0]);
+
+                // Create table header
+                const headerHtml = headers.map(header => `<th>${header}</th>`).join('');
+
+                // Create table rows
+                const rowsHtml = data.map(row => {
+                    const cellsHtml = headers.map(header => `<td>${row[header]}</td>`).join('');
+                    return `<tr>${cellsHtml}</tr>`;
+                }).join('');
+
+                // Combine header and rows to create the table
+                const tableHtml = `
+        <table class="styled-table">
+            <thead>
+                <tr>${headerHtml}</tr>
+            </thead>
+            <tbody>
+                ${rowsHtml}
+            </tbody>
+        </table>
+    `;
+
+                // Apply some basic styling to the table
+                const styledTableHtml = `
+        <style>
+            body {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .styled-table {
+                width: 100%;
+                border-collapse: collapse;
+                font-size: 18px;
+                text-align: left;
+            }
+
+            .styled-table th,
+            .styled-table td {
+                padding: 12px 15px;
+                border-bottom: 1px solid #ddd;
+            }
+
+            .styled-table th {
+                background-color: #f2f2f2;
+            }
+
+            .styled-table tbody tr:nth-child(even) {
+                background-color: #f5f5f5;
+            }
+
+            .table-container {
+                text-align: center;
+            }
+
+            .table-heading {
+                font-size: 24px;
+                font-weight: bold;
+                color: #333;
+                margin-bottom: 15px;
+            }
+        </style>
+        <div class="table-container">
+            <div class="table-heading">תמותה</div>
+            ${tableHtml}
+        </div>
+    `;
+
+                return styledTableHtml;
+            }
+
+
+            // Render the table with a headline
+            const tableHtml = `${generateTableHtml(formattedData)}`;
+            res.send(tableHtml);
+        } catch (error) {
+            console.error('Error connecting to MongoDB:', error);
+            res.status(500).send('<h1>Internal Server Error</h1>');
+        } finally {
+            // Close MongoDB connection
+            client.close();
+        }
     } else {
         // Authentication failed
         res.send('<h1>Login Failed</h1>');
