30bf0f4d3c67782d77e12844952be01a27e84ffb
shmuelstav2 <shmhad123@gmail.com>
2023-12-19 00:37:46 +0200

    new ui

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: tr
 
 // Serve static files (like Excel file)
 // Serve static files (HTML, CSS, etc.)
-app.use(express.static(path.join(__dirname, 'Login_v2')));
+app.use(express.static(path.join(__dirname, 'new')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Login_v2/index.html'));
+    res.sendFile(path.join(__dirname, 'new/index.html'));
 });
 
 app.post('/login', async (req, res) => {
@@ -24,6 +24,114 @@ app.post('/login', async (req, res) => {
     // Check if the provided username and password match the hardcoded values
     if (username === 'lulim' && password === '12345') {
         // Authentication successful
+        res.sendFile(path.join(__dirname, 'new/database.html'));
+    } else {
+        // Authentication failed
+        res.redirect('/new/error.html');
+    }
+});
+
+app.get('/api/tmuta', async (req, res) => {
+    const mongoUri = 'mongodb://localhost:27017/';
+    const dbName = 'lulim';
+    const collectionName = 'tmuta_end';
+
+    const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+
+        // Fetch data from MongoDB excluding _id field
+        const mongoData = await collection.find({}, { projection: { _id: 0 } }).toArray();
+
+        // Format the date as dd/mm/yyyy
+        const formattedData = mongoData.map(item => {
+            if (item.date instanceof Date) {
+                item.date = item.date.toLocaleDateString('en-GB');  // Use 'en-GB' for dd/mm/yyyy format
+            }
+            return item;
+        });
+
+        res.json(formattedData); // Return the data as JSON in the response
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        // Close MongoDB connection
+        client.close();
+    }
+});
+
+app.get('/api/sivuk', async (req, res) => {
+    const mongoUri = 'mongodb://localhost:27017/';
+    const dbName = 'lulim';
+    const collectionName = 'sivuk_end';
+
+    const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+
+        // Fetch data from MongoDB excluding _id field
+        const mongoData = await collection.find({}, { projection: { _id: 0 } }).toArray();
+
+        // Format the date as dd/mm/yyyy
+        const formattedData = mongoData.map(item => {
+            if (item.date instanceof Date) {
+                item.date = item.date.toLocaleDateString('en-GB');  // Use 'en-GB' for dd/mm/yyyy format
+            }
+            return item;
+        });
+
+        res.json(formattedData); // Return the data as JSON in the response
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        // Close MongoDB connection
+        client.close();
+    }
+});
+
+app.get('/api/notru', async (req, res) => {
+    const mongoUri = 'mongodb://localhost:27017/';
+    const dbName = 'lulim';
+    const collectionName = 'notru_end';
+
+    const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+
+        // Fetch data from MongoDB excluding _id field
+        const mongoData = await collection.find({}, { projection: { _id: 0 } }).toArray();
+
+        // Format the date as dd/mm/yyyy
+        const formattedData = mongoData.map(item => {
+            if (item.date instanceof Date) {
+                item.date = item.date.toLocaleDateString('en-GB');  // Use 'en-GB' for dd/mm/yyyy format
+            }
+            return item;
+        });
+
+        res.json(formattedData); // Return the data as JSON in the response
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        // Close MongoDB connection
+        client.close();
+    }
+});
+
+
+/*
         try {
             // Connect to MongoDB
             const mongoUri = 'mongodb://localhost:27017/';
@@ -157,7 +265,7 @@ function generateTableHtml(data) {
 
     return `<table border="1"><thead><tr>${headerHtml}</tr></thead><tbody>${rowsHtml}</tbody></table>`;
 }
-
+*/
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
